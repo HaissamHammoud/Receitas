b37fa14410966b2bd30c91a27eb1cfbeee4d56da
Gabriel Pampuch <gabripampuch@hotmail.com>
2021-06-14 21:12:02 -0300

    adicionando lista de receitas dinamica

--- backend/docs/script.js
+++ backend/docs/script.js
@@ -18,8 +18,6 @@ function clickRecipeDay(){
     document.location.href = newUrl;
 }
 
-
-
 $.getJSON("../data/mainpage.json", function(result){
     document.getElementById('first').innerHTML = result[0].description;
     document.getElementById('second').innerHTML = result[1].description;
@@ -35,4 +33,22 @@ $.getJSON("../data/mainpage.json", function(result){
     document.getElementById('icon1').innerHTML = result[11].tempo;
     document.getElementById('icon2').innerHTML = result[11].rendimento;
     document.getElementById('icon3').innerHTML = result[11].likes;
+});
+
+$.getJSON("../data/recipepage.json", function(data){
+    document.getElementById('n1').innerHTML = data[0].nome;
+    document.getElementById('r1').innerHTML = data[0].resumo;
+    document.getElementById('n2').innerHTML = data[1].nome;
+    document.getElementById('r2').innerHTML = data[1].resumo;
+    document.getElementById('n3').innerHTML = data[2].nome;
+    document.getElementById('r3').innerHTML = data[2].resumo;
+    document.getElementById('n4').innerHTML = data[3].nome;
+    document.getElementById('r4').innerHTML = data[3].resumo;
+    document.getElementById('n5').innerHTML = data[4].nome;
+    document.getElementById('r5').innerHTML = data[4].resumo;
+    document.getElementById('n6').innerHTML = data[5].nome;
+    document.getElementById('r6').innerHTML = data[5].resumo;
+    document.getElementById('n7').innerHTML = data[6].nome;
+    document.getElementById('r7').innerHTML = data[6].resumo;
+
 });
