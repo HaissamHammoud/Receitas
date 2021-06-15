6ab52bc1128e0749f1a11ef874cd4316e69ac59a
Aicha <aichatassa@gmail.com>
2021-06-15 10:14:37 -0300

    calling main and body dinamic in pages with ajax request

--- backend/docs/script.js
+++ backend/docs/script.js
@@ -1,3 +1,62 @@
+const main = document.querySelector('#main1');
+console.log(main);
+
+const body = document.querySelector('#body');
+
+const bodyshow = document.querySelector('#bodyshow');
+fetch('/pages/show-recipe/mainshow.html')
+    .then(res => res.text())
+    .then(texto => {
+        bodyshow.innerHTML = texto;
+    });
+fetch('/pages/listRecipes/mainlist.html')
+    .then(res => res.text())
+    .then(texto => {
+        body.innerHTML = texto;
+        $.getJSON("../data/recipepage.json", function(data){
+            document.querySelector('#n1').innerHTML = data[0].nome;
+            document.querySelector('#r1').innerHTML = data[0].resumo;
+            document.querySelector('#n2').innerHTML = data[1].nome;
+            document.querySelector('#r2').innerHTML = data[1].resumo;
+            document.querySelector('#n3').innerHTML = data[2].nome;
+            document.querySelector('#r3').innerHTML = data[2].resumo;
+            document.querySelector('#n4').innerHTML = data[3].nome;
+            document.querySelector('#r4').innerHTML = data[3].resumo;
+            document.querySelector('#n5').innerHTML = data[4].nome;
+            document.querySelector('#r5').innerHTML = data[4].resumo;
+            document.querySelector('#n6').innerHTML = data[5].nome;
+            document.querySelector('#r6').innerHTML = data[5].resumo;
+            document.querySelector('#n7').innerHTML = data[6].nome;
+            document.querySelector('#r7').innerHTML = data[6].resumo;
+
+        });
+
+    });
+fetch('/pages/home/main.html')
+    .then(res => res.text())
+    .then(texto => {
+        main.innerHTML = texto;
+
+        $.getJSON("../data/mainpage.json", function(result){
+            console.log(result);
+            document.querySelector('#first').innerHTML = result[0].description;
+            document.querySelector('#second').innerHTML = result[1].description;
+            document.querySelector('#third').innerHTML = result[2].description;
+            document.querySelector('#fourth').innerHTML = result[3].description;
+            document.querySelector('#ingred1').innerHTML = result[4].ingrediente;
+            document.querySelector('#ingred2').innerHTML = result[5].ingrediente;
+            document.querySelector('#ingred3').innerHTML = result[6].ingrediente;
+            document.querySelector('#ingred4').innerHTML = result[7].ingrediente;
+            document.querySelector('#ingred5').innerHTML = result[8].ingrediente;
+            document.querySelector('#ingred6').innerHTML = result[9].ingrediente;
+            document.querySelector('#ingred7').innerHTML = result[10].ingrediente;
+            document.querySelector('#icon1').innerHTML = result[11].tempo;
+            document.querySelector('#icon2').innerHTML = result[11].rendimento;
+            document.querySelector('#icon3').innerHTML = result[11].likes;
+        });
+    });
+
+
 function clickHome(){
     let newUrl = "../home/index.html";
     document.location.href = newUrl;
@@ -18,37 +77,5 @@ function clickRecipeDay(){
     document.location.href = newUrl;
 }
 
-$.getJSON("../data/mainpage.json", function(result){
-    document.getElementById('first').innerHTML = result[0].description;
-    document.getElementById('second').innerHTML = result[1].description;
-    document.getElementById('third').innerHTML = result[2].description;
-    document.getElementById('fourth').innerHTML = result[3].description;
-    document.getElementById('ingred1').innerHTML = result[4].ingrediente;
-    document.getElementById('ingred2').innerHTML = result[5].ingrediente;
-    document.getElementById('ingred3').innerHTML = result[6].ingrediente;
-    document.getElementById('ingred4').innerHTML = result[7].ingrediente;
-    document.getElementById('ingred5').innerHTML = result[8].ingrediente;
-    document.getElementById('ingred6').innerHTML = result[9].ingrediente;
-    document.getElementById('ingred7').innerHTML = result[10].ingrediente;
-    document.getElementById('icon1').innerHTML = result[11].tempo;
-    document.getElementById('icon2').innerHTML = result[11].rendimento;
-    document.getElementById('icon3').innerHTML = result[11].likes;
-});
 
-$.getJSON("../data/recipepage.json", function(data){
-    document.getElementById('n1').innerHTML = data[0].nome;
-    document.getElementById('r1').innerHTML = data[0].resumo;
-    document.getElementById('n2').innerHTML = data[1].nome;
-    document.getElementById('r2').innerHTML = data[1].resumo;
-    document.getElementById('n3').innerHTML = data[2].nome;
-    document.getElementById('r3').innerHTML = data[2].resumo;
-    document.getElementById('n4').innerHTML = data[3].nome;
-    document.getElementById('r4').innerHTML = data[3].resumo;
-    document.getElementById('n5').innerHTML = data[4].nome;
-    document.getElementById('r5').innerHTML = data[4].resumo;
-    document.getElementById('n6').innerHTML = data[5].nome;
-    document.getElementById('r6').innerHTML = data[5].resumo;
-    document.getElementById('n7').innerHTML = data[6].nome;
-    document.getElementById('r7').innerHTML = data[6].resumo;
 
-});
